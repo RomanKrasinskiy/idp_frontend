7b8631d8782a1d927b49d9c522fc218e62b3d38e
RomanKrasinskiy <krasinskii_roma@mail.ru>
2024-01-29 03:59:27 +0300

    rename and add some structure header

---
+++ src/components/Header/EditProfile/EditProfile.jsx
@@ -0,0 +1,9 @@
+
+export default function EditProfile() {
+  return (
+    <>
+    
+    </>
+  )
+
+}
--- src/components/Header/HeaderNavigation/HeaderNavigation.jsx
+++
@@ -1,8 +0,0 @@
-
-export const HeaderNavigation = () => {
-  return (
-    <>
-
-    </>
-  )
-}
